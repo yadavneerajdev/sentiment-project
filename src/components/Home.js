8f0143ce12aca4447150943d6d61c48786e49315
yadavneerajdev <connectneerajyadav@gmail.com>
2023-06-03 18:01:47 +0530

    Background blur home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,12 @@ import Images from "/home/nrj-srj/Music/Final Project Application/sentiment-proj
 const Home = () => {
     return (
         <div className='Home-wrapper' style={{ backgroundImage: `url('${Images}')` }}>
-            <h2>
-                Sentiment Analysis
-            </h2>
-            <p>With Public <span style={{ textTransform: "uppercase", fontWeight: "bold", fontSize: "22px", color: "#1a2b3a" }}>Sentiment</span> Nothing Can Fail,<br /> Without it Nothing can succeed.</p>
+            <div className='home-text'>
+                <h2>
+                    Sentiment Analysis
+                </h2>
+                <p>With Public <span style={{ textTransform: "uppercase", fontWeight: "bold", fontSize: "22px", color: "#1a2b3a" }}>Sentiment</span> Nothing Can Fail,<br /> Without it Nothing can succeed.</p>
+            </div>
 
         </div>
     )
