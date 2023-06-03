7c373f30db823e8542ac25806aac5f373554a16e
yadavneerajdev <connectneerajyadav@gmail.com>
2023-06-03 20:26:16 +0530

    new sections updated

--- src/components/UserSection.js
+++ src/components/UserSection.js
@@ -4,11 +4,7 @@ import "./User.css"
 const UserSection = () => {
     return (
         <div className='user-wrapper'>
-            <form>
-                <h3>Upload a csv/json file</h3>
-                <input type="file" />
-                <button>Submit</button>
-            </form>
+
         </div>
     )
 }
